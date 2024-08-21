98728de327989872b111a74c7e1c5af878701cb9
Joshua Addai <kobejoshhtc@gmail.com>
2024-08-21 13:16:31 +0100

    update route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,37 @@
-import React, { useMemo } from 'react';
-import styled from 'styled-components';
-import bg from './Img/bg.png';
-import { MainLayout } from './Styles/Layouts';
-import Orb from './Components/Orb/Orb';
-import Navigation from './Components/Navigation/Navigation';
-import Dashboard from './Components/Dashboard/Dashboard';
-import Income from './Components/Income/Income';
-import Expenses from './Components/Expenses/Expenses';
-import { useGlobalContext } from './Context/globalContext';
+import React, { useMemo } from "react";
+import styled from "styled-components";
+import bg from "./Img/bg.png";
+import { MainLayout } from "./Styles/Layouts";
+import Orb from "./Components/Orb/Orb";
+import Navigation from "./Components/Navigation/Navigation";
+import Dashboard from "./Components/Dashboard/Dashboard";
+import Income from "./Components/Income/Income";
+import Expenses from "./Components/Expenses/Expenses";
+import { useGlobalContext } from "./Context/globalContext";
 import {
   BrowserRouter as Router,
   Route,
   Routes,
   useLocation,
-} from 'react-router-dom';
-import Login from './Components/Login/Login';
-import Signup from './Components/Login/Signup';
-import Goals from './Components/Goals/Goal';
-import DebtTracker from './Components/DebtTracker.js/Debt';
-import WageCalculator from './Components/WageCalc/WageCalc';
-import Home from './Components/Home/Home';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
+} from "react-router-dom";
+import Login from "./Components/Login/Login";
+import Signup from "./Components/Login/Signup";
+import Goals from "./Components/Goals/Goal";
+import DebtTracker from "./Components/DebtTracker.js/Debt";
+import WageCalculator from "./Components/WageCalc/WageCalc";
+import Home from "./Components/Home/Home";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const [active, setActive] = React.useState(1);
-  const location = useLocation()
+  const location = useLocation();
 
   const global = useGlobalContext(); // Ensure this works correctly
 
   const orbMemo = useMemo(() => {
     return <Orb />;
   }, []);
-
-
 
   return (
     <AppStyled bg={bg} className="App">
@@ -49,11 +46,10 @@ function App() {
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="income" element={<Income />} />
             <Route path="expenses" element={<Expenses />} />
-            <Route path="home" element={<Home />} />
+            <Route path="/" element={<Home />} />
             <Route path="goals" element={<Goals />} />
             <Route path="debts" element={<DebtTracker />} />
             <Route path="wageCalc" element={<WageCalculator />} />
-
           </Routes>
         </main>
       </MainLayout>
@@ -63,7 +59,7 @@ function App() {
 
 const AppStyled = styled.div`
   height: 100vh;
-  background-image: url(${props => props.bg});
+  background-image: url(${(props) => props.bg});
   position: relative;
   main {
     flex: 1;
--- frontend/src/Utils/menuItems.js
+++ frontend/src/Utils/menuItems.js
@@ -1,46 +1,54 @@
-import { dashboard, expenses, transactions, trend, goal, loan, wage } from '../Utils/Icons'
+import {
+  dashboard,
+  expenses,
+  transactions,
+  trend,
+  goal,
+  loan,
+  wage,
+} from "../Utils/Icons";
 
 export const menuItems = [
-    {
-        id: 1,
-        title: "Home",
-        icon: transactions,
-        link: "/Home",
-    },
-    {
-        id: 2,
-        title: 'Dashboard',
-        icon: dashboard,
-        link: '/dashboard'
-    },
-    {
-        id: 3,
-        title: "Income Stream",
-        icon: trend,
-        link: "/income",
-    },
-    {
-        id: 4,
-        title: "Spending Snapshot",
-        icon: expenses,
-        link: "/expenses",
-    },
-    {
-        id: 5,
-        title: "Goals Tracker",
-        icon: goal,
-        link: "/goals",
-    },
-    {
-        id: 6,
-        title: " Debt Tracker",
-        icon: loan,
-        link: "/debts",
-    },
-    {
-        id: 7,
-        title: "Wage Calculator",
-        icon: wage,
-        link: "/wageCalc",
-    },
-]
+  {
+    id: 1,
+    title: "Home",
+    icon: transactions,
+    link: "/",
+  },
+  {
+    id: 2,
+    title: "Dashboard",
+    icon: dashboard,
+    link: "/dashboard",
+  },
+  {
+    id: 3,
+    title: "Income Stream",
+    icon: trend,
+    link: "/income",
+  },
+  {
+    id: 4,
+    title: "Spending Snapshot",
+    icon: expenses,
+    link: "/expenses",
+  },
+  {
+    id: 5,
+    title: "Goals Tracker",
+    icon: goal,
+    link: "/goals",
+  },
+  {
+    id: 6,
+    title: " Debt Tracker",
+    icon: loan,
+    link: "/debts",
+  },
+  {
+    id: 7,
+    title: "Wage Calculator",
+    icon: wage,
+    link: "/wageCalc",
+  },
+];
